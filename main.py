95357026a9b7989980a740d2d5753b97128f6156
spenhanley <spenauto@gmail.com>
2016-11-04 15:43:39 +0000

    Find dialog created, sorting out the highlighting when it finds something

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
 import tkinter as tk
 import sys
-from tkinter import filedialog
+import datetime
+import base64
+import zlib
+from tkinter import filedialog, messagebox
+
+
 # TODO: Make the frame match the root node when the window is resized
 # TODO: Make the root title update when it is changed
 # TODO: Implement the actions for the context menu aswell as the remaining menu items
@@ -10,8 +16,11 @@ from tkinter import filedialog
 
 class ShowGUI:
     def __init__(self):
+        self.hide_icon = zlib.decompress(
+            base64.b64decode('eJxjYGAEQgEBBiDJwZDBysAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc='))
         self.root = self.config_root()
         self.frame = self.config_frame(self.root)
+        self.dir = tk.END
         self.text_area = self.text_widget(self.frame)
         self.bind_all()
         self.file = None
@@ -28,7 +37,22 @@ class ShowGUI:
         rt.config(menu=self.make_menu_bar(rt))
         return rt
 
-    def config_frame(self, root):
+    def trigger_find(self, what, dir):
+        pos = '1.0'
+        idx = self.text_area.search(what, tk.INSERT, stopindex=dir)
+        if not idx:
+            self.show_error(what=what) # Break
+        else:
+            pos = '{}+{}c'.format(idx, len(what))
+            self.text_area.tag_config('passed', background='blue')
+            self.text_area.tag_add('passed', 'sel.first', 'sel.last')
+
+    @staticmethod
+    def show_error(what):
+        messagebox.showerror('PyNotepad', 'Cannot find "{}"'.format(what))
+
+    @staticmethod
+    def config_frame(root):
         fr = tk.Frame(root)
         fr.grid(row=0, column=0)
         return fr
@@ -36,26 +60,30 @@ class ShowGUI:
     def bind_all(self):
         self.text_area.bind("<Button-3>", self.popup)
         self.root.bind("<Control-o>", self.open_file)
-        self.text_area.bind("<Control-o>", self.open_file)
+        self.root.bind("<Control-s>", self.save_file)
+        self.root.bind("<Control-S>", self.save_as_file)
+        self.root.bind("<Control-n>", self.reset)
+        self.root.bind("<F5>", self.insert_date)
+        self.root.bind("<Control-f>", self.show_find)
 
     def popup(self, event):
         self.cont_menu.post(event.x_root, event.y_root)
 
-    def save_as_file(self):
-            if self.file is None:
-                self.file = filedialog.asksaveasfilename(**self.file_opts)
-                file = open(self.file, 'w+')
-                file.write(self.text_area.get(0.0, tk.END))
-                print(self.file)
-            else:
-                self.save_file()
+    def save_as_file(self, event):
+        if self.file is None:
+            self.file = filedialog.asksaveasfilename(**self.file_opts)
+            file = open(self.file, 'w+')
+            file.write(self.text_area.get(0.0, tk.END))
+            print(self.file)
+        else:
+            self.save_file(None)
 
-    def save_file(self):
+    def save_file(self, event):
         if self.file:
             file = open(self.file, 'w+')
             file.write(self.text_area.get(0.0, tk.END))
 
-    def open_file(self, arg):
+    def open_file(self, event):
         self.file = filedialog.askopenfilename(**self.file_opts)
         i = 1.0
         if self.file:
@@ -63,13 +91,20 @@ class ShowGUI:
                 self.text_area.insert(1.0, f.read())
                 self.set_title(self.file)
 
-    def reset(self):
+    def reset(self, event):
         self.text_area.delete('1.0', tk.END)
         self.file = None
         self.set_title('Untitled - PyNotepad')
 
     def quit(self):
-        sys.exit(0) # Exit out
+        sys.exit(0)  # Exit out
+
+    # %c and %x do not give the correct time and date formats
+    def insert_date(self, event):
+        date_obj = datetime.datetime.now()
+        date = date_obj.strftime("%d/%m/%Y")
+        time = date_obj.strftime("%H:%M")
+        self.text_area.insert(self.text_area.index(tk.INSERT), time + ' ' + date)
 
     def text_widget(self, frame):
         y_scroll = tk.Scrollbar(frame)
@@ -82,18 +117,57 @@ class ShowGUI:
         x_scroll.config(command=t.xview)
         return t
 
+    def show_find(self, event):
+        fdialog = self.find_dialog()
+
+    def find_dialog(self):
+        xpad = 3
+        ypad = 3
+        dir = ""
+        v = tk.StringVar()
+        top = tk.Tk()
+        top.wm_iconbitmap('image/hidden_icon.ico')
+        top.resizable(False, False)
+        top.wm_title('Find')
+        find_what = tk.Label(top, text='Find what: ')
+        find_what.grid(row=0, column=0, padx=xpad, pady=ypad, sticky=tk.W)
+        to_find = tk.Entry(top)
+        to_find.grid(row=0, column=1, columnspan=2, padx=xpad, pady=ypad)
+        trigger_find = tk.Button(top, text='Find Next', command=lambda: self.trigger_find(to_find.get(), self.dir))
+        trigger_find.grid(row=0, column=3, padx=xpad, pady=ypad)
+        trigger_find.config(width=8)
+        cancel = tk.Button(top, text='Cancel', command=lambda: top.destroy())
+        cancel.grid(row=2, column=3, padx=xpad, pady=ypad)
+        cancel.config(width=8)
+        match = tk.Checkbutton(top, text='Match case')
+        match.grid(row=3, column=0, padx=xpad, pady=ypad)
+        direction_container = tk.LabelFrame(top, text='Direction')
+        direction_container.grid(row=2, column=2, rowspan=2, padx=xpad, pady=ypad)
+        dir_up = tk.Radiobutton(direction_container, text='Up', value=0, command=lambda: self.change_dir('up'))
+        dir_up.grid(row=0, column=0)
+        dir_down = tk.Radiobutton(direction_container, text='Down', value=1, command=lambda: self.change_dir('down'))
+        dir_down.grid(row=0, column=1)
+        return top
+
+    def change_dir(self, dir):
+        if dir == 'up':
+            self.dir = '0.0'
+        else:
+            self.dir = tk.END
+
     # Create the menus
     def make_menu_bar(self, root):
         m = tk.Menu(root, tearoff=0)
         m.add_cascade(label="File", menu=self.file_menu(root))
         m.add_cascade(label="Edit", menu=self.edit_menu(root))
         m.add_cascade(label="Format", menu=self.format_menu(root))
-        m.add_cascade(label="View",   menu=self.view_menu(root))
+        m.add_cascade(label="View", menu=self.view_menu(root))
         m.add_cascade(label="Help", menu=self.help_menu(root))
         return m
 
     def edit_menu(self, root):
         m = tk.Menu(root, tearoff=0)
+        # These items rely on already available key binds, just need to find a way to trigger the events
         m.add_command(label="Undo", accelerator="Ctrl+Z")
         m.add_separator()
         m.add_command(label="Cut", accelerator="Ctrl+X")
@@ -101,23 +175,24 @@ class ShowGUI:
         m.add_command(label="Paste", accelerator="Ctrl+V")
         m.add_command(label="Delete", accelerator="Del")
         m.add_separator()
-        m.add_command(label="Find...", accelerator="Ctrl+F")
+        m.add_command(label="Find...", accelerator="Ctrl+F", command=lambda: self.show_find(None))
         m.add_command(label="Find Next", accelerator="F3")
         m.add_command(label="Replace...", accelerator="Ctrl+H")
         m.add_command(label="Go To...", accelerator="Ctrl+G")
         m.add_separator()
         m.add_command(label="Select All", accelerator="Ctrl+A")
-        m.add_command(label="Time/Date", accelerator="F5") # Adds the current time and date to the document
+        m.add_command(label="Time/Date", accelerator="F5",
+                      command=lambda: self.insert_date(None))  # Adds the current time and date to the document
         return m
 
     def file_menu(self, root):
         m = tk.Menu(root, tearoff=0)
-        m.add_command(label="New", accelerator="Control+N", command=self.reset)
-        m.add_command(label="Open...", accelerator="Ctrl+O", command=self.open_file)
-        m.add_command(label="Save", command=self.save_as_file, accelerator="Ctrl+S")
-        m.add_command(label="Save As...", command=self.save_as_file)
+        m.add_command(label="New", accelerator="Control+N", command=lambda: self.reset(None))
+        m.add_command(label="Open...", accelerator="Ctrl+O", command=lambda: self.open_file(None))
+        m.add_command(label="Save", accelerator="Ctrl+S", command=lambda: self.save_as_file(None))
+        m.add_command(label="Save As...", command=lambda: self.save_as_file(None))
         m.add_separator()
-        m.add_command(label="Print...", accelerator="Ctrl+P") # See todo above
+        m.add_command(label="Print...", accelerator="Ctrl+P")  # ^
         m.add_separator()
         m.add_command(label="Exit", command=self.quit)
         return m
@@ -149,7 +224,7 @@ class ShowGUI:
 
     def view_menu(self, root):
         m = tk.Menu(root, tearoff=0)
-        m.add_command(label="Status Bar") # No idea what this does
+        m.add_command(label="Status Bar")  # No idea what this does
         return m
 
     def set_title(self, title):
